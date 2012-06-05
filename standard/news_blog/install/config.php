179334d6f2dfdbf419d29cfdcf94a16eb2a111b6
Mangirdas <mangirdas@impresspages.org>
2012-06-05 18:32:34 +0100

    installation config

--- standard/news_blog/install/config.php
+++ standard/news_blog/install/config.php
@@ -6,13 +6,92 @@ $languageLong = "English"; //Long title
 $languageUrl = "en";
 
 
-$moduleGroupTitle["standard"] = "standard";
+$moduleGroupTitle["standard"] = "Standard";
 $moduleTitle["standard"]["news_blog"] = "News / Blog";
+  
+  $parameterGroupTitle["standard"]["news_blog"]["admin_translations"] = "Admin translations";
+  $parameterGroupAdmin["standard"]["news_blog"]["admin_translations"] = "1";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["show_read_more"] = "Show read more link";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["show_read_more"] = "Show \"Read more\" link";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["show_read_more"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["show_read_more"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["title_small"] = "Title small";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["title_small"] = "Small";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["title_small"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["title_small"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["title_medium"] = "Title - medium";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["title_medium"] = "Medium";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["title_medium"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["title_medium"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["title_level"] = "Title level";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["title_level"] = "Title size";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["title_level"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["title_level"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["title_big"] = "Title - big";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["title_big"] = "Big";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["title_big"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["title_big"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["pagination"] = "Pagination";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["pagination"] = "Pagination";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["pagination"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["pagination"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["records_per_page"] = "Records per page";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["records_per_page"] = "Records per page";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["records_per_page"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["records_per_page"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["stream"] = "Stream";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["stream"] = "Stream";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["stream"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["stream"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["layout_blog"] = "Layout blog";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["layout_blog"] = "Blog";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["layout_blog"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["layout_blog"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["admin_translations"]["layout_news"] = "Layout news";
+    $parameterValue["standard"]["news_blog"]["admin_translations"]["layout_news"] = "News";
+    $parameterAdmin["standard"]["news_blog"]["admin_translations"]["layout_news"] = "0";
+    $parameterType["standard"]["news_blog"]["admin_translations"]["layout_news"] = "string";
   
   $parameterGroupTitle["standard"]["news_blog"]["translations"] = "Translations";
   $parameterGroupAdmin["standard"]["news_blog"]["translations"] = "0";
 
-    $parameterTitle["standard"]["news_blog"]["translations"]["more"] = "Read more";
-    $parameterValue["standard"]["news_blog"]["translations"]["more"] = "Read more";
-    $parameterAdmin["standard"]["news_blog"]["translations"]["more"] = "0";
-    $parameterType["standard"]["news_blog"]["translations"]["more"] = "lang";
+    $parameterTitle["standard"]["news_blog"]["translations"]["read_more"] = "Read more";
+    $parameterValue["standard"]["news_blog"]["translations"]["read_more"] = "Read more";
+    $parameterAdmin["standard"]["news_blog"]["translations"]["read_more"] = "0";
+    $parameterType["standard"]["news_blog"]["translations"]["read_more"] = "lang";
+  
+  $parameterGroupTitle["standard"]["news_blog"]["widget_IpNewsBlog"] = "Widget IpNewsBlog";
+  $parameterGroupAdmin["standard"]["news_blog"]["widget_IpNewsBlog"] = "1";
+
+    $parameterTitle["standard"]["news_blog"]["widget_IpNewsBlog"]["widget_title"] = "Widget title";
+    $parameterValue["standard"]["news_blog"]["widget_IpNewsBlog"]["widget_title"] = "News / Blog";
+    $parameterAdmin["standard"]["news_blog"]["widget_IpNewsBlog"]["widget_title"] = "0";
+    $parameterType["standard"]["news_blog"]["widget_IpNewsBlog"]["widget_title"] = "string";
+  
+  $parameterGroupTitle["standard"]["news_blog"]["widget_IpNewsBlogBreak"] = "Widget IpNewsBlogBreak";
+  $parameterGroupAdmin["standard"]["news_blog"]["widget_IpNewsBlogBreak"] = "0";
+
+    $parameterTitle["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["intro_break"] = "Intro break";
+    $parameterValue["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["intro_break"] = "*** Intro break ***";
+    $parameterAdmin["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["intro_break"] = "0";
+    $parameterType["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["intro_break"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["widget_title"] = "Widget title";
+    $parameterValue["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["widget_title"] = "Intro break";
+    $parameterAdmin["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["widget_title"] = "0";
+    $parameterType["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["widget_title"] = "string";
+
+    $parameterTitle["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["description"] = "Description";
+    $parameterValue["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["description"] = "This widget marks where to cut an article and place \"Read more\" link in news / blog list. It is visible only in management mode.";
+    $parameterAdmin["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["description"] = "0";
+    $parameterType["standard"]["news_blog"]["widget_IpNewsBlogBreak"]["description"] = "string";

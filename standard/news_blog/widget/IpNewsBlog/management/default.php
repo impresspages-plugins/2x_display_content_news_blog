300ea25c1920dd3c100c9e295da8e8a1d342a9a2
Audrius Jankauskas <audrius@impresspages.org>
2012-06-05 21:28:38 +0300

    Fixed management interface.

--- standard/news_blog/widget/IpNewsBlog/management/default.php
+++ standard/news_blog/widget/IpNewsBlog/management/default.php
@@ -1,6 +1,6 @@
 <div class="ipaStream">
     <label class="ipAdminLabel"><?php echo $this->escPar('standard/news_blog/admin_translations/stream') ?></label>
-    <select name="stream" class="ipAdminInput">
+    <select name="stream" class="ipAdminSelect">
         <?php foreach ($streams as $key => $value) { ?>
         <option <?php echo (isset($stream) && $stream == $value['value']) ? 'selected="selected"' : '' ?> value="<?php echo $this->esc($value['value']) ?>"><?php echo $this->esc($value['title']) ?></option>
         <?php } ?>
@@ -8,7 +8,7 @@
 </div>
 <div class="ipaTitleLevel">
     <label class="ipAdminLabel"><?php echo $this->escPar('standard/news_blog/admin_translations/title_level') ?></label>
-    <select name="titleLevel" class="ipAdminInput">
+    <select name="titleLevel" class="ipAdminSelect">
         <?php foreach ($levels as $key => $level) { ?>
         <option <?php echo ($titleLevel == $level['value']) ? 'selected="selected"' : '' ?> value="<?php echo $this->esc($level['value']) ?>"><?php echo $this->esc($level['title']) ?></option>
         <?php } ?>
@@ -16,15 +16,15 @@
 </div>
 <div class="ipaShowReadMore">
     <label class="ipAdminLabel"><?php echo $this->escPar('standard/news_blog/admin_translations/show_read_more') ?></label>
-    <input type="checkbox" name="showReadMore" class="ipAdminInput" <?php echo isset($showReadMore) && (int)$showReadMore ? 'checked="checked"' : ''; ?>" />
+    <input type="checkbox" name="showReadMore" class="ipAdminCheckbox" <?php echo isset($showReadMore) && (int)$showReadMore ? 'checked="checked"' : ''; ?>" />
 </div>
 <div class="ipaPagination">
     <label class="ipAdminLabel"><?php echo $this->escPar('standard/news_blog/admin_translations/pagination') ?></label>
-    <input type="checkbox" name="pagination" class="ipAdminInput" <?php echo isset($pagination) && (int)$pagination ? 'checked="checked"' : ''; ?>" />
+    <input type="checkbox" name="pagination" class="ipAdminCheckbox" <?php echo isset($pagination) && (int)$pagination ? 'checked="checked"' : ''; ?>" />
 </div>
 <div class="ipaRecordsPerPage">
     <label class="ipAdminLabel"><?php echo $this->escPar('standard/news_blog/admin_translations/records_per_page') ?></label>
-    <input name="recordsPerPage" class="ipAdminInput" value="<?php echo isset($recordsPerPage) ? $recordsPerPage : ''; ?>" type="number"/>
+    <input name="recordsPerPage" class="ipAdminInput" value="<?php echo isset($recordsPerPage) ? $recordsPerPage : ''; ?>" type="number" />
 </div>
 
 
